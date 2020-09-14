ee82150868d8c5318717943ebbd55c52d51e767a
qingjiawen <1503927265@qq.com>
2020-09-14 22:45:04 +0800

    final修改

--- src/com/finaldemo/FinalDemo.java
+++ src/com/finaldemo/FinalDemo.java
@@ -5,14 +5,27 @@ package com.finaldemo;
  */
 public class FinalDemo {
     int age;
-    final String NAME;
+    final String NAME;//只能账值一次
 
-    //构造器 构造方法
+    //可以声明的时候直接赋值  也可以用构造器 构造方法
     public FinalDemo() {
         NAME = null;
+        age=20;
     }
 
     public static void main(String[] args) {
+        FinalDemo fd=new FinalDemo();
+      //  fd.NAME="ddd";
 
+        //局部变量 用final修饰同样不能再次赋值 可以把赋值与声明分开
+        //注意事项 局部变量注意一定要在使用之前赋值
+        final  int a;
+        a=10;
+        //a=20
+        System.out.println(a);
+
+        //final 修饰的局部 引用类型 只能指向一个地址 但不影响fd2 里的方法赋值使用
+        final  FinalDemo fd2=new FinalDemo();
+        //fd2=new FinalDemo();
     }
 }
